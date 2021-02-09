e23d9042c633a50da9b6dc2e5bf1444eb56ebc44
Kelcey Vehanen <kvehanen@pixelrebirth.com>
2021-02-08 22:19:36 -0800

    Added some notes to psake build

--- psakefile.ps1
+++ psakefile.ps1
@@ -13,3 +13,9 @@ Task Testing {
     $pester.FailedCount -eq 0 -AND 
         [int]$($pester.CodeCoverage.HitCommands.Count / $pester.CodeCoverage.NumberOfCommandsAnalyzed * 100) -ge 90
 }
+
+# Add conditional builds for Build(test)/Minor(dev)/Major(prod)
+# Include DB version control to not risk losing prod
+# Consider migration of data, even from a regeneration perspective may improve maintainability
+# Version control with Major.Minor.Build format in PSD1 file
+# Squash Commit on Release
