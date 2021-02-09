66110b6dc44a9ef006ee00ac57f4c5c73bb11c53
Kelcey Vehanen <kvehanen@pixelrebirth.com>
2021-02-08 19:36:44 -0800

    Auto-2-8-2021-7-36-44-PM

--- psake.ps1
+++ psake.ps1
@@ -1,13 +1,13 @@
 Import-Module Psake
 
-Task default -depends Testing
+Task default -depends Commit
 
-Task Testing -Depends Commit {
-    Invoke-Pester -CodeCoverage ./Libraries/*.ps1
-}
-
-Task Commit {
+Task Commit -Depends Testing {
     git add -A
     git commit -m "Auto-$((get-date) -replace(" |:|/","-"))"
     git push
+}
+
+Task Testing {
+    Invoke-Pester -CodeCoverage ./Libraries/*.ps1
 }
