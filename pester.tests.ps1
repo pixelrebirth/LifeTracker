23a27f82f8224d8b5d0c622f7beac2ca2161d6ea
Kelcey Vehanen <kvehanen@pixelrebirth.com>
2021-02-21 01:05:20 -0800

    Pester Updated, boo

--- pester.tests.ps1
+++ pester.tests.ps1
@@ -12,34 +12,34 @@ describe "LifeTrackerFunctions" {
     }
 }
 
+Remove-Item $script:DatabaseLocation -Force -ErrorAction 0
 describe "LifeTrackerCmdlets" {
-    Remove-Item $script:DatabaseLocation -Force -ErrorAction 0
     it "Should create a database for the App" {
-        New-TaskletDatabase | should be $true
+        New-TaskletDatabase | Should -BeTrue
     }
 
     it "Add-Tasklet uploads a document to DB" {
-        Add-Tasklet -Value "Leadership" -Title "Another Tasklet" -Tags "Test,123"| Should be "Tasklet Saved"
-        Add-Tasklet -Value "Systemic" -Title "Testing 123" | Should be "Tasklet Saved"
+        Add-Tasklet -Value "Leadership" -Title "Another Tasklet" -Tags "Test,123"| Should -Be "Tasklet Saved"
+        Add-Tasklet -Value "Systemic" -Title "Testing 123" | Should -Be "Tasklet Saved"
     }
     
     it "Get-Tasklet returns created tasklet" {
-        (Get-Tasklet -FormatView -Tags "Test").title | Should Be "Another Tasklet"
-        (Get-Tasklet -Value "Systemic").title | Should Be "Testing 123"
+        (Get-Tasklet -FormatView -Tags "Test").title | Should -Be "Another Tasklet"
+        (Get-Tasklet -Value "Systemic").title | Should -Be "Testing 123"
     }
     
     Mock Read-Host {return "3"}
     Mock Write-Host {}
     it "Register-TaskletTouch allocates 50 to Weight when Read-Host is 3" {
-        (Register-TaskletTouch)[0].Weight | should be "50"
+        (Register-TaskletTouch)[0].Weight | Should -Be "50"
     }
     
     it "Register-TaskletTouch should filter down to only value/tag if asked" {
-        Register-TaskletTouch -Value "Creativity" -Tags "Test" | Should Be "No Tasklets Found"
+        Register-TaskletTouch -Value "Creativity" -Tags "Test" | Should -Be "No Tasklets Found"
     }
     
     it "Should archive the tasklet created above" {
-        (Get-Tasklet -Value "Systemic") | Complete-Tasklet | Should Be "Tasklet [Testing 123] Completed"
+        (Get-Tasklet -Value "Systemic") | Complete-Tasklet | Should -Be "Tasklet [Testing 123] Completed"
     }
 
     it "Should create a character in Database and Validate it" {
@@ -50,18 +50,18 @@ describe "LifeTrackerCmdlets" {
 describe "LifeTrackerClasses" {
     $tasklet = [tasklet]::new("Test Title","Creativity")
     it "Should instantiate a tasklet class" {
-        $tasklet.title | should be "Test Title"
-        $tasklet.CreatedOn | Should Not Be Null
-        $tasklet.UpdatedOn | Should Not Be Null
+        $tasklet.title | Should -Be "Test Title"
+        $tasklet.CreatedOn | Should -Not -Be Null
+        $tasklet.UpdatedOn | Should -Not -Be Null
     }
 
     $rewardlet = [rewardlet]::new("Reward Me",5,8)
     it "Should instantiate a rewardlet class" {
-        $rewardlet.title | should be "Reward Me"
+        $rewardlet.title | Should -Be "Reward Me"
     }
 
     it "Should create a character in the DB" {
         $character = [character]::new()
-        $character.Dharma | Should Be 100
+        $character.Dharma | Should -Be 100
     }
 }
